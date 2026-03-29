a69051439077ee14acbb3f77e987d82849b16080
Vansh Yadav <vanshyadavbca.sspu@gmail.com>
2026-03-29 14:44:17 +0530

    Fix indentation in dockerPush.groovy

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,6 +1,6 @@
 def call (String project , String ImageTag , String dockerHubUser){
   withCredentials([usernamePassword(credentialsId: "dockerHubCred", passwordVariable: "dockerHubPass", usernameVariable: "dockerHubUser")]) {
   sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
-}
   sh " docker push ${dockerHubUser}/${project}:${ImageTag}"
+}
 }
